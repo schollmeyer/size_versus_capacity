ae9b2be5a9cbad514cad3951fd902c7940b89cb9
schollmeyer <georg.schollmeyer@stat.uni-muenchen.de>
2025-02-18 16:49:23 +0100

    gs

--- experiments_2.R
+++ experiments_2.R
@@ -1,10 +1,10 @@
 set.seed(1234567)
-CT <- oofos:::compute_random_context(100,40,p=0.9)
+CT <- oofos:::compute_random_context(75,25,p=0.9)
 d <- gurobi(oofos::compute_extent_vc_dimension(CT))$objval
 
-L <- oofos:::compute_concept_lattice(CT)
-
-null_distribution <- get_null_distribution(L,n_rep=1000)
+concept_lattice <- oofos:::compute_concept_lattice(CT)
+s <- nrow(concept_lattice$extents)
+null_distribution <- get_null_distribution(concept_lattice,n_rep=1000)
 
 plot(ecdf(null_distribution))
 
