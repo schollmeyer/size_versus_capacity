e036a72a31473b0371a6898156e61bb2bae71e4e
Georg Schollmeyer <69206011+schollmeyer@users.noreply.github.com>
2025-02-18 21:41:53 +0100

    Update functions.R

--- functions.R
+++ functions.R
@@ -1,7 +1,8 @@
-get_null_distribution <- function(concept_lattice=NULL,context=NULL,n_rep){
+get_null_distribution <- function(concept_lattice=NULL,context=NULL,n_rep,outputflag=0){
   n <- ncol(concept_lattice$extents)
+  if(!is.null(context)){n <- nrow(context)}
   result <- rep(0,n_rep)
- if(!is.null(context){model <- oofos::optimize_on_context_extents,context,objective=rep(0,n))}	
+ if(!is.null(context)){model <- oofos::optimize_on_context_extents(context,objective=rep(0,n))}	
   for(k in (1:n_rep)){
     objective <- sample(c(-1,1),size=n,replace=TRUE)
     objective <- oofos::compute_objective(data.frame(y=objective),"y","1")
@@ -10,7 +11,7 @@ get_null_distribution <- function(concept_lattice=NULL,context=NULL,n_rep){
     }
   else{
 	model$obj[(1:n)] <- objective
-	result[k] <- gurobi(model)$objval  
+	result[k] <- gurobi(model,params=list(outputflag=outputflag))$objval  
 	  }
   }
 return(result)}
